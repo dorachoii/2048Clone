050561024fab1528a58f0b38edc6d7a63d189fe2
Dora Choi <hellodoradora7@gmail.com>
2025-12-18 18:26:01 +0900

    Feat: SpawnRandomTile()

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -120,7 +120,7 @@ public class BoardManager : MonoBehaviour
                 }
             }
 
-
+            
         }
     }
 
--- Assets/Scripts/TileSpawner.cs
+++ Assets/Scripts/TileSpawner.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TileSpawner : MonoBehaviour
@@ -16,6 +17,7 @@ public class TileSpawner : MonoBehaviour
         }
         SpawnTileAt(2, 2);
         SpawnTileAt(3, 3);
+        SpawnRandomTile();
     }
 
     void SpawnTileAt(int row, int col)
@@ -23,7 +25,31 @@ public class TileSpawner : MonoBehaviour
         Vector2 pos = BoardManager.Instance.CelltoWorld(row, col);
         GameObject tileObj = Instantiate(tilePrefab, pos, Quaternion.identity);
         
+        // value값도 나중에 조정되어야 함
         Tile tile = new Tile(2, new Vector2Int(row, col), tileObj);
         BoardManager.Instance.board[row, col] = tile;
+    }
+
+    public void SpawnRandomTile()
+    {
+        List<Vector2Int> emptyCells = new List<Vector2Int>();
+
+        // instance가 아니라 class에 직접 접근하는게 맞는지
+        for(int r = 0; r < BoardManager.row; r++)
+        {
+            for(int c = 0; c < BoardManager.col; c++)
+            {
+                if(BoardManager.Instance.board[r,c] == null || BoardManager.Instance.board[r,c].value != 0)
+                {
+                    emptyCells.Add(new Vector2Int(r,c));
+                }
+            }
+        }
+
+        if(emptyCells.Count == 0) return;
+
+        Vector2Int chosenCell = emptyCells[Random.Range(0, emptyCells.Count)];
+
+        SpawnTileAt(chosenCell.x, chosenCell.y);
     }
 }
